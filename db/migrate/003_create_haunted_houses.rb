f4943457fb93834e02e41ec32128e029ea1a9af6
George Blackmore <gtblackmore@gmail.com>
2020-03-06 04:50:15 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,7 +5,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :location
       t.string :theme
-      t.integer :price
+      t.float :price
       t.boolean :family_friendly
       t.string :opening_date
       t.string :closing_date
