574bea6a3ade5ce6165455be655231348189356e
George Blackmore <gtblackmore@gmail.com>
2020-03-06 04:39:54 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,8 +5,8 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
       t.integer :price
       t.string :size
       t.string :image_url
-      t.timestamps :created_at
-      t.timestamps :updated_at
+      t.datetime :created_at
+      t.datetime :updated_at
     end
   end 
 end 
