c1a2d16dd60329502e84e4b7653eca9e645508ec
George Blackmore <gtblackmore@gmail.com>
2020-03-06 04:15:00 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,6 +10,10 @@
 
 class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
-    create_table
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.string :size
+      t.
   end 
 end 
