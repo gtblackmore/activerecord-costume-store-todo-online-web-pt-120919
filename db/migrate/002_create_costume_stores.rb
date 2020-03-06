2278c72760e31e30fed5e434cb5e476003efbdb7
George Blackmore <gtblackmore@gmail.com>
2020-03-06 04:20:31 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,6 +6,7 @@ class CostumeStores < ActiveRecord::Migration[5.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :number_of_employees
-      t.boolean :
+      t.boolean :still_in_business
+      t.
 
 end 
