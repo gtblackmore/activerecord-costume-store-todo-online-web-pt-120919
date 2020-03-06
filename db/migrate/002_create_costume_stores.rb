5c0cd051f60bf894c8b440c94abe0d14ae19651e
George Blackmore <gtblackmore@gmail.com>
2020-03-06 04:19:15 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,6 +1,11 @@
 # Create your costume_stores migration here
 class CostumeStores < ActiveRecord::Migration[5.2]
   def change
-    create table :Costumetore
+    create table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.boolean :
 
 end 
